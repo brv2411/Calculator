d39cabe5de0127ef15db252f0445aa406e63e8ee
brv2411 <brv2411@gmail.com>
2015-10-04 09:11:42 +0800

    I did some simple comments

---
+++ Calculator.py
@@ -0,0 +1,3 @@
+#This is my calculator. We will be using multi-line statements, print, input, variables, exit and if, else and elif statements
+#The aim of this project is to help learning Python programmers programme a basic calculator, so remember nothing too fancy
+
